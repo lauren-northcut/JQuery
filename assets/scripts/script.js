2bd6acabe2dc24a713ce12124db48481c30fef05
Lauren <TechnologicalType@MacBook-Air-2.local>
2021-06-13 06:54:26 -0400

    added functionality to abt btns

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
   // Single Click
   //   Code
   $("#single").click(function () {
---
+++ assets/scripts/script3.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $("ui").slice(2).css("color", "lightBlue");
+  });
